ecaa4f1e046d96a94f9717d8d005b3445a1a223e
aohanhe2 <13823904000@139.com>
2018-10-03 17:17:36 +0800

    创建项目

---
+++ src/main/java/swallow/framework/Swallow3WebserverFrameworkApplication.java
@@ -0,0 +1,12 @@
+package swallow.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swallow3WebserverFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swallow3WebserverFrameworkApplication.class, args);
+	}
+}
